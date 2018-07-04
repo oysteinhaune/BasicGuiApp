8ad5eaee7cc783bf59627d46ad49c0749b224a94
oystein <oysteinha@gmail.com>
2018-07-04 22:57:37 +0200

    Ny scene med tabell med dummy data lagt til.

---
+++ src/sample/Person.java
@@ -0,0 +1,47 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+import java.time.LocalDate;
+
+public class Person {
+    private SimpleStringProperty firstName, lastName;
+    private LocalDate birthday;
+
+    public Person(String firstName, String lastName, LocalDate birthday) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.birthday = birthday;
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public SimpleStringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public SimpleStringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
--- src/sample/tabellenController.java
+++ src/sample/tabellenController.java
@@ -1,15 +1,31 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ResourceBundle;
 
 public class tabellenController {
+
+    //configure the table
+    @FXML private TableView<Person> tableView;
+    @FXML private TableColumn<Person, String> firstNameColumn;
+    @FXML private TableColumn<Person, String> lastNameColumn;
+    @FXML private TableColumn<Person, LocalDate> birthdayColumn;
 
     public void objectButton(ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -21,5 +37,29 @@ public class tabellenController {
 
         window.setScene(tableViewScene);
         window.show();
+    }
+
+    @FXML
+    public void initialize() {
+        // set up the columns in the table
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+        birthdayColumn.setCellValueFactory(new PropertyValueFactory<Person, LocalDate>("birthday"));
+
+        // load dummy date
+        tableView.setItems(getPeople());
+    }
+
+    // Will return an ObservableList of People objects
+    public ObservableList<Person> getPeople() {
+        ObservableList<Person> people = FXCollections.observableArrayList();
+        people.add(new Person("Øystein", "Haugnes Aune", LocalDate.of(1988, Month.MAY, 13)));
+        people.add(new Person("Frank", "Sinatra", LocalDate.of(1915, Month.DECEMBER, 12)));
+        people.add(new Person("Elvis", "Presley", LocalDate.of(1935, Month.JANUARY, 8)));
+        people.add(new Person("Linus", "Torvalds", LocalDate.of(1969, Month.DECEMBER, 28)));
+        people.add(new Person("Bill", "Gates", LocalDate.of(1955, Month.OCTOBER, 28)));
+        people.add(new Person("Mark", "Zuckerberg", LocalDate.of(1984, Month.MAY, 14)));
+
+        return people;
     }
 }
